276266e917eb8155f9a4a95a39b3dd3ccd3f2789
kirill0909 <kournovk81@gmail.com>
2022-09-03 18:49:23 +0300

    RemoveDoubles function has been changed to work with generics

--- remove_dublicates.go
+++ remove_dublicates.go
@@ -1,16 +1,14 @@
 package slices
 
-/*
-RemoveDublicatesStr removes duplicatesw from []string
-*/
-func RemoveDublicatesStr(arr []string) []string {
-  allKeys := make(map[string]bool)
-  list := []string{}
-  for _, item := range arr {
-    if _, value := allKeys[item]; !value {
-      allKeys[item] = true
-      list = append(list, item)
-    }
-  }
-  return list
+// RemoveDublicatesStr removes duplicatesw from []T
+func RemoveDublicates[T comparable](arr []T) []T {
+	allKeys := make(map[T]bool)
+	list := []T{}
+	for _, item := range arr {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
 }
--- remove_dublicates_test.go
+++ remove_dublicates_test.go
@@ -1,27 +1,46 @@
 package slices
 
 import (
-	"testing"
 	"reflect"
-
+	"testing"
 )
 
 func TestRemoveDublicatesStr(t *testing.T) {
-  type testData struct {
-    arr []string
-    want []string
-  }
+	type testData struct {
+		arr  []string
+		want []string
+	}
 
-  tests := []testData{
-    {[]string{"one", "two", "three", "one"}, []string{"one", "two", "three"}},
-    {[]string{"1", "2", "3", "4", "1", "2"}, []string{"1", "2", "3", "4"}},
-    {[]string{}, []string{}},
-  }
+	tests := []testData{
+		{[]string{"one", "two", "three", "one"}, []string{"one", "two", "three"}},
+		{[]string{"1", "2", "3", "4", "1", "2"}, []string{"1", "2", "3", "4"}},
+		{[]string{}, []string{}},
+	}
 
-  for _, test := range tests {
-    got := RemoveDublicatesStr(test.arr)
-    if !reflect.DeepEqual(got, test.want) {
-      t.Errorf("RemoveDublicatesStr([]string) []string, want: %v, got: %v\n", test.want, got)
-    }
-  }
+	for _, test := range tests {
+		got := RemoveDublicates(test.arr)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("RemoveDublicatesStr([]string) []string, want: %v, got: %v\n", test.want, got)
+		}
+	}
+}
+
+func TestRemoveDublicatesInt(t *testing.T) {
+	type testData struct {
+		arr  []int
+		want []int
+	}
+
+	tests := []testData{
+		{[]int{1, 2, 3, 1}, []int{1, 2, 3}},
+		{[]int{1, 2, 3, 4, 1, 2}, []int{1, 2, 3, 4}},
+		{[]int{}, []int{}},
+	}
+
+	for _, test := range tests {
+		got := RemoveDublicates(test.arr)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("RemoveDublicatesStr([]string) []string, want: %v, got: %v\n", test.want, got)
+		}
+	}
 }
