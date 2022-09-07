b64c6efece0c4544fdeacb4c8a3f99a325ea95b0
kirill0909 <kournovk81@gmail.com>
2022-09-07 13:00:27 +0300

    Changed login of RemoveByIndex function

--- remove_by_index.go
+++ remove_by_index.go
@@ -6,11 +6,11 @@ import (
 )
 
 // RemoveByIndex remove element from slice []T by index
-func RemoveByIndex[T comparable](arr []T, index int)   error {
-  if len(arr) >= index {
-    arr = append(arr[:index], arr[index+1:]...)
-    return nil
-  }
-  
-  return  errors.New(fmt.Sprintf("index not found %v", index))
+func RemoveByIndex[T comparable](arr *[]T, index int) error {
+	if len(*arr) >= index {
+		(*arr) = append((*arr)[:index], (*arr)[index+1:]...)
+		return nil
+	}
+
+	return errors.New(fmt.Sprintf("index not found %v", index))
 }
